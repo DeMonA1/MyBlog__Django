063c6ec58dc3810742b88572d6a4a9e112c5db34
DeMonA1 <demonagrail1@gmail.com>
2024-10-10 12:34:41 +0300

    View post_list as a class

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 # Create your views here.
 
@@ -33,3 +34,15 @@ def post_detail(request, post, day, month, year):
         'blog/post/detail.html',
         {'post':post}
     )
+    
+    
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+    
+    
