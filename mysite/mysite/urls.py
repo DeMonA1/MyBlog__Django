fbd57652f82f75f72d106432a33f333e2ac29d14
DeMonA1 <demonagrail1@gmail.com>
2024-10-13 08:12:15 +0300

    Sitemap edit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -26,6 +26,6 @@ sitemaps = {'posts': PostSitemap}
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
-    path('sitemap.xml', sitemap,{'sitemaps': sitemaps},
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
             name='django.contrib.sitemaps.views.sitemap')
 ]
